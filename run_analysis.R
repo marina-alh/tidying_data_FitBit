4aa7b0acfbc44f1f7191fe26fc1af1abd0442ff4
Marina Holanda <marinaandradeholanda@gmail.com>
2022-09-08 18:39:23 +0200

    done

--- run_analysis.R
+++ run_analysis.R
@@ -2,14 +2,30 @@ library(tidyverse)
 library(ggplot2)
 library(readr)
 library(data.table)  
+setwd("data")
 
-# Dplyr verbs reminders: select, filter, arrange, mutate, summarize(subfunctions)
+# Dplyr verbs reminders: select(look up the special), filter, arrange, mutate, summarize(subfunctions)
 # Dplyr func: bind_rows,bind_columns   
 # Tidyr reminders: 
 #               gather >> multiple columns into 1
 #               separate >> separate values into multiple columns
 #               spread >> spread one column into multiple
-
+# Fixing character vectors:
+#                       toupper()
+#                       tolower()
+#                       strsplit() - to split variable names
+#                       sub() - to substitute a character. Only looks for the 1srt appearence 
+#                       gsub() - replace all instances of the character
+#                       grep() - look for strings in variable or variable names
+#                       grepl() - returns TRUE or FALSE
+#                       nchar() - number of characters
+#                       substre() - take part of the string
+#                       paste(), paste0()
+#
+# Regular Expressions reminders:
+#                       
+#
+#
 
 url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
@@ -25,10 +41,17 @@ download.file(url, destfile = "data//dataset.zip")
 # data <- do.call(cbind, temp)
 
 
+## TASK 1: Merges the training and the test sets to create one data set.
+
 Xtrain_df <- read_delim("UCI HAR Dataset/train/X_train.txt", col_names = FALSE)
 ytrain_df <- read_tsv("UCI HAR Dataset/train/y_train.txt", col_names = FALSE)
+subject_train <- read_tsv("UCI HAR Dataset/train/subject_train.txt", col_names = FALSE)
+
+
 Xtest_df <- read_delim("UCI HAR Dataset/test/X_test.txt", col_names = FALSE)
 ytest_df <- read_tsv("UCI HAR Dataset/test/y_test.txt", col_names = FALSE)
+subject_test <- read_tsv("UCI HAR Dataset/test/subject_test.txt", col_names = FALSE)
+
 
 tbl_df(Xtest_df)
 tbl_df(Xtrain_df)
@@ -68,13 +91,66 @@ aux2 <- Xtest_df %>%
 
 Xdf <- bind_rows(aux,aux2)
 ydf <- bind_rows(ytrain_df,ytest_df)
+subjectdf<- bind_rows(subject_train,subject_test)
+colnames(subjectdf) <- "SUBJECT"
 
 features = tbl_df(features)
 features = t(features) #transpose
 
-rm(aux,aux2,ytest_df,ytrain_df,Xtest_df,Xtrain_df)
 
 colnames(Xdf) <- features[1,]
 
 
 colnames(Xdf)
+
+
+
+
+
+# TASK 2: Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+
+Xdf <- Xdf %>% 
+        select(contains(c("mean()","std()")))
+
+
+# TASK 3: Uses descriptive activity names to name the activities in the data set
+
+colnames(ydf) <- "ACTIVITIES"
+
+ydf <- ydf %>% 
+        mutate(ACTIVITIES = case_when(
+                ACTIVITIES == 1 ~ "WALKING",
+                ACTIVITIES == 2 ~ "WALKING_UPSTAIRS",
+                ACTIVITIES == 3 ~ "WALKING_DOWNSTAIRS",
+                ACTIVITIES == 4 ~ "SITTING",
+                ACTIVITIES == 5 ~ "STANDING",
+                ACTIVITIES == 6 ~ "LAYING",
+        ))
+
+
+final_df <- bind_cols(ydf, Xdf)
+
+
+# TASK 4: Appropriately labels the data set with descriptive variable names. 
+
+
+
+colnames(final_df)<- gsub("[0-9]","",colnames(final_df)) %>% trimws()
+        
+final_df <- bind_cols(subjectdf,final_df)
+
+final_df
+
+
+rm(aux,aux2,actv,features,Xdf,Xtest_df,ydf,ytest_df,Xtrain_df,ytrain_df,subject_test,subject_train,subjectdf)
+
+
+# TASK 5: From the data set in step 4, creates a second, independent tidy data set with 
+#               the average of each variable 
+#               for each activity and each subject.
+
+summary_data <- final_df %>% 
+        group_by(SUBJECT,ACTIVITIES) %>% 
+        summarise_all(mean, na.rm=TRUE)
+
