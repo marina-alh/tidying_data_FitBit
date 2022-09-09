a7d40a4f058f10d78d4d51e8e060e09152a97123
Marina Holanda <marinaandradeholanda@gmail.com>
2022-09-09 18:43:44 +0200

    replaced read tsv with read table

--- run_analysis.R
+++ run_analysis.R
@@ -34,106 +34,79 @@ download.file(url, destfile = "dataset.zip")
 
 
 
-
-
-Xtrain_df <- read_delim("UCI HAR Dataset/train/X_train.txt", col_names = FALSE)
-ytrain_df <- read_tsv("UCI HAR Dataset/train/y_train.txt", col_names = FALSE)
-subject_train <- read_tsv("UCI HAR Dataset/train/subject_train.txt", col_names = FALSE)
-
-
-Xtest_df <- read_delim("UCI HAR Dataset/test/X_test.txt", col_names = FALSE)
-ytest_df <- read_tsv("UCI HAR Dataset/test/y_test.txt", col_names = FALSE)
-subject_test <- read_tsv("UCI HAR Dataset/test/subject_test.txt", col_names = FALSE)
+features <- read.table("UCI HAR Dataset/features.txt") 
+features = features$V2
 
 
 
-features <- read_tsv("UCI HAR Dataset/features.txt", col_names = FALSE)
+Xtrain_df <- read.table("UCI HAR Dataset/train/X_train.txt") 
+colnames(Xtrain_df) <- features
+ytrain_df <- read.table("UCI HAR Dataset/train/y_train.txt") %>% rename("activities" = V1)
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt") %>% rename("subject" = V1)
+
+train_dataset <- bind_cols(subject_train,ytrain_df,Xtrain_df)
 
 
 
 
+Xtest_df <- read.table("UCI HAR Dataset/test/X_test.txt")
+colnames(Xtest_df) <- features
+ytest_df <- read.table("UCI HAR Dataset/test/y_test.txt") %>% rename("activities" = V1)
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt") %>% rename("subject" = V1)
 
-# TASK1: Merge train and test df by columns will not work. They have different number of columns 
-
-
-
-Xtrain_df <- Xtrain_df %>% 
-        select(1:561)
-        
-        
-Xtest_df <- Xtest_df %>% 
-        select(1:561) 
-
-
-# Merging clean train and test data sets in Xdf
-
-Xdf <- bind_rows(Xtrain_df,Xtest_df) # Xdf contains all observations
-ydf <- bind_rows(ytrain_df,ytest_df) # ydf contains all activities labeled by their numbers
-
-#merging train and test subject column
-
-subjectdf<- bind_rows(subject_train,subject_test) 
-colnames(subjectdf) <- "SUBJECT"
-
-
-
-features = tbl_df(features)
-features = t(features) #transpose
-
-
-# making the features data set as Variable Names 
-colnames(Xdf) <- features[1,]
+test_dataset <- bind_cols(subject_test,ytest_df,Xtest_df)
 
 
 
 
+
+
+# TASK 1: Marging train and test datasets
+
+dataset <- bind_rows(train_dataset,test_dataset)
 
 
 
 # TASK 2: Extracts only the measurements on the mean and standard deviation for each measurement. 
 
 
-Xdf <- Xdf %>% 
-        select(contains(c("mean()","std()")))
+dataset <- dataset %>% 
+        
+        select(contains(c("subject","activities","mean()","std()")))
 
 
 # TASK 3: Uses descriptive activity names to name the activities in the data set
 
-colnames(ydf) <- "ACTIVITIES"
 
-ydf <- ydf %>% 
-        mutate(ACTIVITIES = case_when(
-                ACTIVITIES == 1 ~ "WALKING",
-                ACTIVITIES == 2 ~ "WALKING_UPSTAIRS",
-                ACTIVITIES == 3 ~ "WALKING_DOWNSTAIRS",
-                ACTIVITIES == 4 ~ "SITTING",
-                ACTIVITIES == 5 ~ "STANDING",
-                ACTIVITIES == 6 ~ "LAYING",
+dataset <- dataset %>% 
+        mutate(activities = case_when(
+               activities == 1 ~ "WALKING",
+               activities == 2 ~ "WALKING_UPSTAIRS",
+               activities == 3 ~ "WALKING_DOWNSTAIRS",
+               activities == 4 ~ "SITTING",
+               activities == 5 ~ "STANDING",
+               activities == 6 ~ "LAYING",
         ))
-
-
-final_df <- bind_cols(ydf, Xdf)
 
 
 # TASK 4: Appropriately labels the data set with descriptive variable names. 
 
 
 
-colnames(final_df)<- gsub("[0-9]","",colnames(final_df)) %>% trimws()
+colnames(dataset)<- gsub("[0-9]","",colnames(dataset)) %>% trimws()
         
-final_df <- bind_cols(subjectdf,final_df)
 
 
 
-rm(aux,aux2,url,features,Xdf,Xtest_df,ydf,ytest_df,Xtrain_df,ytrain_df,subject_test,subject_train,subjectdf,not_all_na)
+rm(features,Xtest_df,ytest_df,Xtrain_df,ytrain_df,subject_test,subject_train,train_dataset,test_dataset)
 
 
 # TASK 5: From the data set in step 4, creates a second, independent tidy data set with 
 #               the average of each variable 
 #               for each activity and each subject.
 
-summary_data <- final_df %>% 
-        group_by(SUBJECT,ACTIVITIES) %>% 
+summary_data <- dataset%>% 
+        group_by(subject,activities) %>% 
         summarise_all(mean, na.rm=TRUE)
 
 
